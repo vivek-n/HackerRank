4decfe511fe5dcd627c97f60a2950e6d38c7d09d
Vivek Nagaraj <nagaraj.vivek@gmail.com>
2016-11-22 10:22:14 +0530

    Create conditional-statements.cpp

---
+++ cpp/introduction/conditional-statements.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int num;
+    
+    cin >> num;
+    
+    if(num == 1)
+        cout << "one" << endl;
+    
+    else if (num == 2)
+        cout << "two" << endl;
+        
+    else if (num == 3)
+        cout << "three" << endl;
+    
+    else if (num == 4)
+        cout << "four" << endl;
+    
+    else if (num == 5)
+        cout << "five" << endl;
+    
+    else if (num == 6)
+        cout << "six" << endl;
+    
+    else if (num == 7)
+        cout << "seven" << endl;
+    
+    else if (num == 8)
+        cout << "eight" << endl;
+        
+    else if (num == 9)
+        cout << "nine" << endl;
+    
+    else
+        cout << "Greater than 9" << endl;
+ 
+   return 0;
+}
