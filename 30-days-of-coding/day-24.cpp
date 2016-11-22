d41c91d57d631e4b91924d340983a45e0d9ff804
Vivek Nagaraj <nagaraj.vivek@gmail.com>
2016-11-22 10:56:24 +0530

    Create day-24.cpp

---
+++ 30-days-of-coding/day-24.cpp
@@ -0,0 +1,28 @@
+          Node* removeDuplicates(Node *head)
+          {
+              //Write your code here
+              Node *ptr1, *ptr2, *dup;
+              ptr1 = head;
+
+              while(ptr1 != NULL && ptr1->next != NULL)
+              {
+                 ptr2 = ptr1;
+
+                 while(ptr2->next != NULL)
+                 {
+                   if(ptr1->data == ptr2->next->data)
+                   {
+                      dup = ptr2->next;
+                      ptr2->next = ptr2->next->next;
+                      free(dup);
+                   }
+                   else
+                   {
+                      ptr2 = ptr2->next;
+                   }
+                 }
+                 ptr1 = ptr1->next;
+              }
+              
+              return head;
+          }
