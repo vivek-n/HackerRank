e4b4f80f4db120afd45073ad08f250cc1c9d22f0
Vivek Nagaraj <nagaraj.vivek@gmail.com>
2016-11-23 12:12:05 +0530

    Create print-pretty.cpp

---
+++ cpp/stl/print-pretty.cpp
@@ -0,0 +1,5 @@
+		/* Enter your code here */
+
+        cout << setw(0) << left << showbase << hex << nouppercase << (long)A << endl;
+        cout << setw(0xf) << right << setfill('_') << showpos << setprecision(2) << fixed << B << endl;
+        cout << setw(0xf) << left << noshowpos << scientific << uppercase << setprecision(9) << C << endl;
